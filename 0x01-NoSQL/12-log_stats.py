c082e1f25bb8702972923941cde422cfdeeed1fe
Dani-1138 <105964577+Dani-1138@users.noreply.github.com>
2023-10-17 23:30:48 +0300

    log_stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,39 +1,23 @@
-#!/usr/bin/env python3
-"""
-Provide some stats about Nginx logs stored in MongoDB
-Database: logs, Collection: nginx, Display same as example
-first line: x logs, x number of documents in this collection
-second line: Methods
-5 lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-one line with method=GET, path=/status
-"""
 from pymongo import MongoClient
 
+# Connect to MongoDB
+client = MongoClient()
+db = client.logs
+collection = db.nginx
 
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+# Get total number of logs
+total_logs = collection.count_documents({})
 
+# Get counts for each HTTP method
+http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+method_counts = {method: collection.count_documents({"method": method}) for method in http_methods}
 
-def log_stats(mongo_collection, option=None):
-    """
-    Prototype: def log_stats(mongo_collection, option=None):
-    Provide some stats about Nginx logs stored in MongoDB
-    """
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
+# Get count for method=GET and path=/status
+status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
 
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
-    print("Methods:")
-    for method in METHODS:
-        log_stats(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
-    print(f"{status_check} status check")
-
-
-if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    log_stats(nginx_collection)
+# Display the results
+print(f"{total_logs} logs")
+print("Methods:")
+for method in http_methods:
+    print(f"    method {method}: {method_counts[method]}")
+print(f"{status_check_count} status check")
